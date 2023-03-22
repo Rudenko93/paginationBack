f654a233f92f80e9597e1a5fbd68acdcebdf85f2
Deni <dv04111993@yandex.ru>
2023-03-22 18:48:13 +0100

    react-pagination

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,8 +1,9 @@
 export const Pagination: React.FC = () => {
   return (
     <div className="pagination-wrapper">
-      <div className="pagination">
+      <div className="pagination1">
         <a href="#">&laquo;</a>
+        <a href="#">&lt;</a>
         <a href="#">1</a>
         <a className="active" href="#">
           2
@@ -11,6 +12,7 @@ export const Pagination: React.FC = () => {
         <a href="#">4</a>
         <a href="#">5</a>
         <a href="#">6</a>
+        <a href="#">&gt;</a>
         <a href="#">&raquo;</a>
       </div>
     </div>
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,7 +3,9 @@ import { HomeProvider } from "../../contexts/HomeContext"
 import { CardList } from "../../components/CardList"
 import { Controls } from "../../components/Controls"
 import { Pagination } from "../../components/Pagination"
+import ReactPaginate from "react-paginate"
 import { ICard } from "../../types"
+import axios from "axios"
 
 export type Status = "error" | "loading" | "success"
 
@@ -16,20 +18,25 @@ export const Home = () => {
   const [limit, setLimit] = useState<string>("20")
   const [limitDisabled, setLimitDisabled] = useState<boolean>(false)
 
+  const [pageCount, setPageCount] = useState<number>(0)
+
   useEffect(() => {
     const fetchCards = async () => {
       try {
-        const res = await fetch(
+        const res = await axios(
           `http://localhost:5000/cards${limit && `?&_limit=${limit}`}${
             search && `&q=${search}`
           }`
         )
-        if (!res.ok) {
+
+        if (!res.status) {
           setStatus("error")
           throw new Error("failed to fetch")
         } else {
-          const data = await res.json()
-          setCards(data)
+          setCards(res.data)
+          setPageCount(
+            Math.ceil(parseInt(res.headers["x-total-count"]) / parseInt(limit))
+          )
           setStatus("success")
         }
       } catch (error: any) {
@@ -52,7 +59,21 @@ export const Home = () => {
         <div className="container">
           <Controls />
           <CardList cards={cards} status={status} />
-          <Pagination />
+          <ReactPaginate
+            pageCount={pageCount}
+            initialPage={1}
+            pageRangeDisplayed={2}
+            marginPagesDisplayed={1}
+            onPageChange={() => ""}
+            containerClassName="pagination"
+            breakLinkClassName="page-num"
+            activeLinkClassName="page-active"
+            pageLinkClassName="page-num"
+            nextLinkClassName="page-num"
+            previousLinkClassName="page-num"
+            previousLabel="&lt;"
+            nextLabel="&gt;"
+          />
         </div>
       </div>
     </HomeProvider>
